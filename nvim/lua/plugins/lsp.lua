b466a43bdd4f1cc2ee6b057257dc88c0857a11c5
kewang <364517893@qq.com>
2023-05-01 15:04:52 +0800

    find the way to config lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -13,11 +13,20 @@ require("mason-lspconfig").setup({
   ensure_installed = {
     "lua_ls",
     "clangd",
+    "pyright",
   },
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("lspconfig").lua_ls.setup {
+  capabilities = capabilities,
+}
+
+require("lspconfig").pyright.setup {
+  capabilities = capabilities,
+}
+
+require("lspconfig").clangd.setup {
   capabilities = capabilities,
 }
